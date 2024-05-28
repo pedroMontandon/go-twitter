3937aa367e61595b428d3de86b40f6cda31a3f8b
pedroMontandon <pedrobarbosa89@gmail.com>
2024-05-28 15:23:54 -0300

    mux added in main.go

--- main.go
+++ main.go
@@ -4,7 +4,18 @@ import (
 	"net/http"
 )
 
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./")))
-	http.ListenAndServe(":8080", nil)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/healthz", healthCheckHandler)
+	mux.Handle("/app/assets/login.png", http.FileServer(http.Dir("./")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("./"))))
+
+	http.ListenAndServe(":8080", mux)
 }
