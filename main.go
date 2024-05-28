beefa850849ef794d69137fd0f257bf59b11eb0b
pedroMontandon <pedrobarbosa89@gmail.com>
2024-05-28 15:45:25 -0300

    metric middleware has been implemented

--- main.go
+++ main.go
@@ -2,7 +2,21 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 )
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+var config = apiConfig{}
+
+func accessCounterMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		config.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
 
 func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -10,12 +24,28 @@ func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
+func metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits: " + strconv.Itoa(config.fileserverHits)))
+}
+
+func resetMetricsHandler(w http.ResponseWriter, r *http.Request) {
+	config.fileserverHits = 0
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Metrics reset"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/healthz", healthCheckHandler)
-	mux.Handle("/app/assets/login.png", http.FileServer(http.Dir("./")))
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("./"))))
+	mux.HandleFunc("GET /metrics", metricsHandler)
+	mux.HandleFunc("POST /reset", resetMetricsHandler)
+	mux.Handle("GET /app/assets/login.png", accessCounterMiddleware(http.FileServer(http.Dir("./"))))
+	mux.Handle("GET /app/", accessCounterMiddleware(http.StripPrefix("/app/", http.FileServer(http.Dir("./")))))
+
+	mux.HandleFunc("POST /healthz", healthCheckHandler)
 
 	http.ListenAndServe(":8080", mux)
 }
