e9621856f407e27dbaa4ecbdbbaca5e9be35a77d
pedroMontandon <pedrobarbosa89@gmail.com>
2024-05-29 09:57:15 -0300

    metric route has been refactored: now it has its own html

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
-	"strconv"
 )
 
 type apiConfig struct {
-	fileserverHits int
+	FileserverHits int
 }
 
 var config = apiConfig{}
 
 func accessCounterMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		config.fileserverHits++
+		config.FileserverHits++
 		next.ServeHTTP(w, r)
 	})
 }
@@ -31,13 +31,27 @@ func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func metricsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hits: " + strconv.Itoa(config.fileserverHits)))
+	// w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	// w.WriteHeader(http.StatusOK)
+	// w.Write([]byte("Hits: " + strconv.Itoa(config.FileserverHits)))
+	tmpl, err := template.ParseFiles("pages/metrics.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	data := apiConfig{
+		FileserverHits: config.FileserverHits,
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func resetMetricsHandler(w http.ResponseWriter, r *http.Request) {
-	config.fileserverHits = 0
+	config.FileserverHits = 0
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Metrics reset"))
@@ -46,12 +60,12 @@ func resetMetricsHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /metrics", metricsHandler)
-	mux.HandleFunc("POST /reset", resetMetricsHandler)
-	mux.Handle("GET /app/assets/login.png", accessCounterMiddleware(http.FileServer(http.Dir("./"))))
+	mux.HandleFunc("GET /api/reset", resetMetricsHandler)
 	mux.Handle("GET /app/", accessCounterMiddleware(http.StripPrefix("/app/", http.FileServer(http.Dir("./")))))
+	mux.Handle("GET /app/assets/logo.png", accessCounterMiddleware(http.FileServer(http.Dir("./"))))
+	mux.HandleFunc("GET /admin/metrics", metricsHandler)
 
-	mux.HandleFunc("GET /healthz", healthCheckHandler)
+	mux.HandleFunc("GET /api/healthz", healthCheckHandler)
 
 	http.ListenAndServe(":8080", mux)
 }
